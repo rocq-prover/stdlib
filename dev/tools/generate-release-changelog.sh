5e0a6e868a7dfcff4066ec3e7cf5880c964e0e12
Pierre Roux <pierre.roux@onera.fr>
2025-03-12 16:00:42 +0100

    Update changelog generation script

--- dev/tools/generate-release-changelog.sh
+++ dev/tools/generate-release-changelog.sh
@@ -3,21 +3,6 @@
 set -e
 set -o pipefail
 
-if [ $# != 1 ]; then
-    echo "Usage: $0 BRANCH"
-    exit
-fi
-
-branch=$1
-
-# Set SLOW_CONF to have the confirmation output wait for a newline
-# Emacs doesn't send characters until the RET so we can't quick_conf
-if [ -z ${SLOW_CONF+x} ] || [ -n "$INSIDE_EMACS" ]; then
-    quick_conf=(-n 1)
-else
-    quick_conf=()
-fi
-
 ask_confirmation() {
     read -p "Continue anyway? [y/N] " "${quick_conf[@]}" -r
     echo
@@ -31,80 +16,26 @@ if ! git diff --quiet; then
     ask_confirmation
 fi
 
-remote=$(git config --get "branch.${branch}.remote" || true)
-
-if [ -z "$remote" ]; then
-    echo "Warning: branch $branch has no associated remote."
-    ask_confirmation
-else
-
-    if [[ "$remote" != $(git config --get "branch.master.remote") ]]; then
-        echo "Warning: branch master and branch $branch do not have the same remote."
-        ask_confirmation
-    fi
-
-    official_remote_git_url="git@github.com:coq/coq"
-    official_remote_https_url="github.com/coq/coq"
-    remote_url=$(git remote get-url "$remote" --all)
-
-    if [ "$remote_url" != "${official_remote_git_url}" ] && \
-           [ "$remote_url" != "${official_remote_git_url}.git" ] && \
-           [ "$remote_url" != "https://${official_remote_https_url}" ] && \
-           [ "$remote_url" != "https://${official_remote_https_url}.git" ] && \
-           [[ "$remote_url" != "https://"*"@${official_remote_https_url}" ]] && \
-           [[ "$remote_url" != "https://"*"@${official_remote_https_url}.git" ]] ; then
-        echo "Warning: remote $remote does not point to the official Rocq repo,"
-        echo "that is $official_remote_git_url"
-        echo "It points to $remote_url instead."
-        ask_confirmation
-    fi
-
-    git fetch "$remote"
-
-    if [[ $(git rev-parse master) != $(git rev-parse "${remote}/master") ]]; then
-        echo "Warning: branch master is not up-to-date with ${remote}/master."
-        ask_confirmation
-    fi
-
-    if [[ $(git rev-parse "$branch") != $(git rev-parse "${remote}/${branch}") ]]; then
-        echo "Warning: branch ${branch} is not up-to-date with ${remote}/${branch}."
-        ask_confirmation
-    fi
-
-fi
-
-git checkout "$branch" --detach > /dev/null 2>&1
 changelog_entries_with_title=(doc/changelog/*/*.rst)
-git checkout master > /dev/null 2>&1
 
 tmp=$(mktemp)
 for f in "${changelog_entries_with_title[@]}"; do
-    if ! [ -f "$f" ]; then
-        >&2 echo "Warning: $f is missing in master branch."
-        continue
-    fi
 
     cat=${f%/*} # dirname
     if [[ ${f##*/} = 00000-title.rst ]]; then
         type=0
     else
-        type_name=$(head -n 1 "$f" | cut -f 2 -d ' ')
-        type_name=${type_name%":**"}
-        type_name=${type_name#"**"}
-        case "$type_name" in
-            Changed) type=1;;
-            Removed) type=2;;
-            Deprecated) type=3;;
-            Added) type=4;;
-            Fixed) type=5;;
-            *) >&2 echo "Unknown changelog type $type_name in $f"; type=6;;
-        esac
+        type_name=$(head -n 1 "$f" | cut -f 3 -d ' ')
+        type_name=${type_name%".v\`"}
+        type_name=${type_name#"\`"}
+        type="1${type_name}"
     fi
     printf '%s %s %s\n' "$cat" "$type" "$f" >> "$tmp"
 done
 
 while read -r _ type f; do
     cat "$f" >> released.rst
+    echo "" >> released.rst
     if ! [[ $type = 0 ]]; then git rm "$f" >> /dev/null; fi
 done < <(sort "$tmp")
 
