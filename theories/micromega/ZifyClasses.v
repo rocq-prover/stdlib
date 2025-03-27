dc34c3a5001ddca2fe2c20d714acc2c040d25c18
Frédéric Besson <frederic.besson@inria.fr>
2025-03-27 16:16:13 +0100

    [zify] Notation from Zify -> Tify

--- theories/micromega/ZifyClasses.v
+++ theories/micromega/ZifyClasses.v
@@ -1,3 +1,14 @@
 Attributes deprecated(since="9.1", note="Use TifyClasses instead.").
 
 From Stdlib Require Export TifyClasses.
+
+Notation InjTyp    := TifyClasses.InjTyp.
+Notation BinOp     := TifyClasses.BinOp.
+Notation UnOp      := TifyClasses.UnOp.
+Notation CstOp     := TifyClasses.CstOp.
+Notation BinRel    := TifyClasses.BinRel.
+Notation PropOp    := TifyClasses.PropOp.
+Notation PropUOp   := TifyClasses.PropUOp.
+Notation BinOpSpec := TifyClasses.BinOpSpec.
+Notation UnOpSpec  := TifyClasses.UnOpSpec.
+Notation Saturate  := TifyClasses.Saturate.
