8e7d18245c05919eb00828d814af797929e99e0d
Andres Erbsen <andres@andres.systems>
2025-05-29 23:04:17 -0400

    Clean up subcomponent dependencies of lia

--- test-suite/bugs/bug_4187.v
+++ test-suite/bugs/bug_4187.v
@@ -7,7 +7,8 @@ Axiom proof_admitted : False.
 Tactic Notation "admit" := case proof_admitted.
 From Stdlib Require Import List.
 From Stdlib Require Import Setoid.
-From Stdlib Require Import BinNat.
+From Stdlib Require Import BinNat .
+From Stdlib Require Compare_dec Wf_nat.
 From Stdlib Require Import Sumbool.
 Global Set Implicit Arguments.
 Global Generalizable All Variables.
