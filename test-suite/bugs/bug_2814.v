aed91a74d041ce8bcfec984851e3e12d9ad34f5f
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-07-11 17:22:01 +0200

    Adapt to rocq-prover/rocq#20902 (induction checks the right scheme)

--- test-suite/bugs/bug_2814.v
+++ test-suite/bugs/bug_2814.v
@@ -1,6 +1,7 @@
 From Stdlib Require Import Program.
 
-Goal forall (x : Type) (f g : Type -> Type) (H : f x ~= g x), False.
+Goal forall (x : Type) (f g : Type -> Type) (H : JMeq (f x) (g x)) (H:f x), g x.
   intros.
-  Fail induction H.
-Abort.
+  induction H.
+  assumption.
+Qed.
