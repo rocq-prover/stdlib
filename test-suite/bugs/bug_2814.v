944b2230b113453178495e0e0620116ec30bbb77
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-09-04 14:42:57 +0200

    Update test-suite/bugs/bug_2814.v
    Co-authored-by: Olivier Laurent <30601497+olaure01@users.noreply.github.com>

--- test-suite/bugs/bug_2814.v
+++ test-suite/bugs/bug_2814.v
@@ -1,6 +1,6 @@
 From Stdlib Require Import Program.
 
-Goal forall (x : Type) (f g : Type -> Type) (H : JMeq (f x) (g x)) (H:f x), g x.
+Goal forall (x : Type) (f g : Type -> Type) (H : JMeq (f x) (g x)), f x -> g x.
   intros.
   induction H.
   assumption.
