6a504379358883bcf80b1db12ceb1c57df11c2de
Pierre Roux <pierre.roux@onera.fr>
2026-04-03 13:47:41 +0200

    Rename micromega plugin tactics to avoid conflicts
    For instance conflicts between two libraries, one that would still be
    using the plugin shipped with Rocq and another that uses the new plugin.

--- theories/micromega/Psatz.v
+++ theories/micromega/Psatz.v
@@ -34,9 +34,9 @@ Ltac nia := Lia.nia.
 
 Ltac xpsatz dom d :=
   let tac := lazymatch dom with
-  | Z => (xsos_Z Lia.zchecker) || (xpsatz_Z d Lia.zchecker)
-  | R => (xsos_R Lra.rchecker) || (xpsatz_R d Lra.rchecker)
-  | Q => (xsos_Q Lqa.rchecker) || (xpsatz_Q d Lqa.rchecker)
+  | Z => (mp_sos_Z Lia.zchecker) || (mp_psatz_Z d Lia.zchecker)
+  | R => (mp_sos_R Lra.rchecker) || (mp_psatz_R d Lra.rchecker)
+  | Q => (mp_sos_Q Lqa.rchecker) || (mp_psatz_Q d Lqa.rchecker)
   | _ => fail "Unsupported domain"
   end in tac.
 
